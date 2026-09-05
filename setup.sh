349c5e1e691b30f6f8c4bd8e155cdec960a5f4d3
kekkaimahou <ggforgamingplayer+dbxvbcg4@gmail.com>
2026-09-05 09:39:28 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ yes | pkg upgrade
 pkg install tur-repo
 pkg update
 pkg upgrade -y
-pkg install python python3.11 git
+pkg install -y python python3.11 git
 git clone https://github.com/kekkaimahou/tokimeki.git
 cd tokimeki
 pip3.11 install -r requirements.txt
