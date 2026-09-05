9e17969b6893c134bb223871a5a69904b21c0e10
kekkaimahou <ggforgamingplayer+dbxvbcg4@gmail.com>
2026-09-05 09:33:53 -0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+clear
+pkg update
+yes | pkg upgrade
+pkg install tur-repo
+pkg update
+pkg upgrade -y
+pkg install python python3.11
+git clone https://github.com/kekkaimahou/tokimeki.git
+cd tokimeki
+pip3.11 install -r requirements.txt
